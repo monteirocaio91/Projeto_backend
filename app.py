848da0975412aa63a79ccc784414868eacecf181
monteirocaio91 <garcia.monteiro.caio@gmail.com>
2025-06-04 08:19:09 -0300

    c

--- app.py
+++ app.py
@@ -1,61 +1,13 @@
-from flask import Flask, render_template, request, redirect
-import logging
+from flask import Flask
+from routes.main_routes import main_bp
+from routes.usuario_routes import usuario_bp
 
 app = Flask(__name__)
-logging.basicConfig(level=logging.INFO)
+app.secret_key = 'yahoo'
 
-USERS = {
-    "": ""
-}
+# registrar os blueprints
+app.register_blueprint(main_bp)
+app.register_blueprint(usuario_bp, url_prefix='/usuario')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html', error=None)
-
-@app.route('/acesso', methods=['POST'])
-def acesso():
-    username = request.form['username']
-    password = request.form['password']
-
-    if username in USERS and USERS[username] == password:
-        return redirect('/livros')
-    else:
-        logging.warning(f'Tentativa de login inválido: {username}')
-        return render_template('login.html', error="Usuário ou senha incorretos")
-
-@app.route('/livros')
-def livros():
-    return render_template('livros.html')
-
-@app.route('/cadastro')
-def cadastro():
-    return render_template('cadastro.html', error=None)
-
-@app.route('/cadastro', methods=['POST'])
-def cadastro_post():
-    username = request.form['username']
-    password = request.form['password']
-    confirm_password = request.form['confirm_password']
-
-    if not username or not password:
-        error = "Preencha todos os campos."
-        return render_template('cadastro.html', error=error)
-
-    if password != confirm_password:
-        error = "As senhas não conferem."
-        return render_template('cadastro.html', error=error)
-
-    if username in USERS:
-        error = "Usuário já cadastrado."
-        return render_template('cadastro.html', error=error)
-
-    USERS[username] = password
-    logging.info(f'Novo usuário cadastrado: {username}')
-    return redirect('/livros')
-
-if __name__ == '__main__':
+if __name__ =='__main__':
     app.run(debug=True)
