c1b84a6ab189279d24d52aa3d7681463a170c700
monteirocaio91 <garcia.monteiro.caio@gmail.com>
2025-06-11 11:46:20 -0300

    c

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from routes.user_routes import main_bp
-from routes.main_routes import usuario_bp
+from routes.main_routes import main_bp
+from routes.usuario_routes import usuario_bp
 
 app = Flask(__name__)
 app.secret_key = 'yahoo'
 
-# registrar os blueprints
+# Registrar os blueprints
 app.register_blueprint(main_bp)
-app.register_blueprint(usuario_bp, url_prefix='/user')
+app.register_blueprint(usuario_bp, url_prefix='/usuario')
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
